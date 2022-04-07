58e426a50bf576268468aff5c6ff210632af130b
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2022-04-07 21:35:51 +0300

    Proje oluşturuldu
    Proje oluşturuldu ve Kullanıcı arayüzleri dosyaları oluşturuldu

---
+++ BlogApp/BlogApp/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlogApp
+//
+//  Created by Burak Emre gündeş on 7.04.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .link
+    }
+
+
+}
+
